0049de8d72965a7bda0c447af82b65732c6b9d28
Michelle Mukami <you@example.com>
2019-07-15 10:14:55 +0300

    changes made in app.module.ts

--- github-search/src/app/app.module.ts
+++ github-search/src/app/app.module.ts
@@ -10,8 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './about/about.component';
 import { PageErrorComponent } from './page-error/page-error.component';
-// import { UpperCasePipe } from './upper-case.pipe';
-// import { TextColorChangeDirective } from './text-color-change.directive';
+
 
 @NgModule({
   declarations: [
@@ -19,8 +18,7 @@ import { PageErrorComponent } from './page-error/page-error.component';
     ProfileComponent,
     AboutComponent,
     PageErrorComponent,
-    // UpperCasePipe,
-    // TextColorChangeDirective,
+    
   ],
   imports: [
     BrowserModule,
